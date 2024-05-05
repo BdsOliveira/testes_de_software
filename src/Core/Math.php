630424fa72357e24f5cc0ec00608aca87c0f18b0
Bruno Oliveira <bds.commus@gmail.com>
2024-05-05 18:53:42 -0300

    add base math implementation

---
+++ src/Core/Math.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Core;
+
+class Math
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function subtract($a, $b)
+    {
+        return $a - $b;
+    }
+
+    public function multiply($a, $b)
+    {
+        return $a * $b;
+    }
+
+    public function divide($a, $b)
+    {
+        if ($b === 0) {
+            return 0;
+        }
+        return $a / $b;
+    }
+
+    public function power($a, $b)
+    {
+        return $a ** $b;
+    }
+
+    public function sqrt($a)
+    {
+        return sqrt($a);
+    }
+}
